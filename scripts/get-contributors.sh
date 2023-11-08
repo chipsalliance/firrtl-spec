db7e78ec73c41ce5bc14aaa93d1bb5dc1ca5d18e
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2023-11-07 20:26:22 -0500

    [nfc] Fix contributors script to fetch more
    Fix the contributors script so that it will fetch more than the default 30
    contributors.  We now have more than 30 total contributors!
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- scripts/get-contributors.sh
+++ scripts/get-contributors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-curl -X GET https://api.github.com/repos/chipsalliance/firrtl-spec/contributors | \
+curl -X GET https://api.github.com/repos/chipsalliance/firrtl-spec/contributors?per_page=100 | \
   jq '.[].login' | \
   sort -u | \
   sed 's/"\(.*\)"/- [`@\1`](https:\/\/github.com\/\1)/'

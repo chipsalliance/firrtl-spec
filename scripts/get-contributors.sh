bed38a7659bddbc199549274d41ed0cdf45d518d
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2024-02-10 18:00:22 -0500

    [nfc] Update contributors script to take token
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- scripts/get-contributors.sh
+++ scripts/get-contributors.sh
@@ -1,5 +1,39 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-curl -X GET https://api.github.com/repos/chipsalliance/firrtl-spec/contributors?per_page=100 | \
+set -eo pipefail
+
+usage() {
+  cat <<EOF
+USAGE: $0 [options]
+
+Download JSON representing the first 100 contributors to the firrtl-spec
+repository.
+
+OPTIONS:
+    -g                    An optional GitHub token to authenticate with
+    -h                    Display available options
+EOF
+}
+
+OPT_GITHUB_TOKEN=
+while getopts "g:h" option; do
+  case $option in
+    g)
+      OPT_GITHUB_TOKEN=$OPTARG
+      ;;
+    h)
+      usage
+      exit 0
+  esac
+done
+
+CURL_ARGS=
+if [[ $OPT_GITHUB_TOKEN ]]; then
+  CURL_ARGS="-H \"Authorization: Bearer $OPT_GITHUB_TOKEN\""
+fi
+
+RESPONSE=$(eval curl $CURL_ARGS https://api.github.com/repos/chipsalliance/firrtl-spec/contributors?per_page=100)
+
+echo $RESPONSE | \
   jq '.[] | {login, html_url}' | \
   jq -s 'sort_by(.login) | {contributors: .}'
